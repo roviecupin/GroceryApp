f005ed0b9fbaff3df1db1e7f5e98eb2d2ee2210e
roviecupin <rovie.cupin@normi.edu.ph>
2025-10-19 22:10:28 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\1.MyGrocery_app-20251009T080758Z-1-001\\MyGrocery_app\\grocery_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\1.MyGrocery_app-20251009T080758Z-1-001\\MyGrocery_app\\grocery_app"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\1.MyGrocery_app-20251009T080758Z-1-001\\MyGrocery_app\\grocery_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\1.MyGrocery_app-20251009T080758Z-1-001\\MyGrocery_app\\grocery_app"
+  "FLUTTER_TARGET=D:\\1.MyGrocery_app-20251009T080758Z-1-001\\MyGrocery_app\\grocery_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuOA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZWRhZGE3YzU2ZQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZWYwY2QwMDA5MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\1.MyGrocery_app-20251009T080758Z-1-001\\MyGrocery_app\\grocery_app\\.dart_tool\\package_config.json"
+)
