f9c41567201482b81f32a70eaee391afc2ee3840
roviecupin <rovie.cupin@normi.edu.ph>
2025-10-19 23:07:07 +0800

    Update product.dart

--- lib/product.dart
+++ lib/product.dart
@@ -1,1 +1,20 @@
+class Product {
+  final String id;
+  final String name;
+  final String vendor;
+  final double price;
+  final String imageUrl; 
 
+  Product({
+    required this.id,
+    required this.name,
+    required this.vendor,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  @override
+  String toString() {
+    return 'Product(id: $id, name: $name, vendor: $vendor, price: $price, imageUrl: $imageUrl)';
+  }
+}
