10207f603c2b64bb5b6bc5f13cf0ae577e21c0da
roviecupin <rovie.cupin@normi.edu.ph>
2025-10-19 22:06:12 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/app_drawer.dart';
+import 'package:grocery_app/info.dart';
+import 'package:grocery_app/newsstand.dart';
+import 'package:grocery_app/shop.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(      
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {'/shop': (context) => const Shop(),
+              '/newsstand': (context) => const Newsstand(),
+              '/info': (context) => const Info(),
+              },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+ @override
+ Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('My Grocery'),
+    ),
+    drawer: AppDrawer(),
+    body: Center(),
+  );
+ }
+}
