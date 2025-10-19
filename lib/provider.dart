e0519ac468fd6b2f093fb5c4faf42ae3f6d09e39
roviecupin <rovie.cupin@normi.edu.ph>
2025-10-19 23:19:00 +0800

    Update provider.dart

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:my_grocery_app/product.dart';
 
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem({required this.product, this.quantity = 1});
+}
+
+class CartProvider extends ChangeNotifier {
+  final Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => _items;
+
+  List<CartItem> get cartItems => _items.values.toList();
+
+  int get itemCount => _items.length;
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.product.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items[product.id]!.quantity++;
+    } else {
+      _items[product.id] = CartItem(product: product);
+    }
+    notifyListeners();
+  }
+
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) return;
+
+    if (_items[productId]!.quantity > 1) {
+      _items[productId]!.quantity--;
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+}
